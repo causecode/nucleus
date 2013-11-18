9b3cb80cf8b20b9936e2873beceadb48acefd742
Shashank Agrawal <shashank.agrawal@causecode.com>
2013-11-18 12:25:25 +0530

    Adding core classes for user & role.

---
+++ grails-app/domain/com/cc/user/Role.groovy
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2011, CauseCode Technologies Pvt Ltd, India.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are not permitted.
+ */
+
+package com.cc.user
+
+class Role {
+
+    String authority
+
+    static mapping = {
+        cache true
+    }
+
+    static constraints = {
+        authority blank: false, unique: true
+    }
+
+}
---
+++ grails-app/domain/com/cc/user/User.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2011, CauseCode Technologies Pvt Ltd, India.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are not permitted.
+ */
+
+package com.cc.user
+
+class User {
+
+    transient springSecurityService
+
+    String username
+    String password
+    boolean enabled = true
+    boolean accountExpired
+    boolean accountLocked
+    boolean passwordExpired
+
+    static transients = ['springSecurityService']
+
+    static constraints = {
+        username blank: false, unique: true
+        password blank: false
+    }
+
+    static mapping = {
+        password column: '`password`'
+    }
+
+    Set<Role> getAuthorities() {
+        UserRole.findAllByUser(this).collect { it.role } as Set
+    }
+
+    def beforeInsert() {
+        encodePassword()
+    }
+
+    def beforeUpdate() {
+        if (isDirty('password')) {
+            encodePassword()
+        }
+    }
+
+    protected void encodePassword() {
+        password = springSecurityService.encodePassword(password)
+    }
+
+}
