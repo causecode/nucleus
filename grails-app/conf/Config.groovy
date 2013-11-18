8e60436fc78964efdc3f745a59fc8916a880e4d3
Shashank Agrawal <shashank.agrawal@causecode.com>
2013-11-18 13:37:40 +0530

    Released first version.

---
+++ grails-app/conf/Config.groovy
@@ -0,0 +1,5 @@
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.cc.user.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.cc.user.UserRole'
+grails.plugins.springsecurity.authority.className = 'com.cc.user.Role'
