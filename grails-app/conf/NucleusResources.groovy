a982e18438101dabc04be55912fa5790cdd32367
Shashank Agrawal <shashank.agrawal@causecode.com>
2013-11-19 11:15:11 +0530

    Added plugin attribute to resolve css & js files.

--- grails-app/conf/NucleusResources.groovy
+++ grails-app/conf/NucleusResources.groovy
@@ -1,19 +1,19 @@
 modules = {
     nucleusCore {
-        resource url: [dir: 'js', file: "application.js"]
+        resource url: [dir: 'js', file: "application.js", plugin: "nucleus"]
     }
     validation {
-        resource url: [dir: "js", file: "jquery.validate.js"]
-        resource url: [dir: "js", file: "jquery.validate.additional-methods.js"]
-        resource url: [dir: "js", file: "jquery.validate.instance.js"]
+        resource url: [dir: "js", file: "jquery.validate.js", plugin: "nucleus"]
+        resource url: [dir: "js", file: "jquery.validate.additional-methods.js", plugin: "nucleus"]
+        resource url: [dir: "js", file: "jquery.validate.instance.js", plugin: "nucleus"]
     }
     datetimePicker {
-        resource url: [dir: "css/datepicker", file: "bootstrap-datepicker.css"]
-        resource url: [dir: "css/datepicker", file: "jquery.timepicker.css"]
-        resource url: [dir: "js/datepicker", file: "bootstrap-datepicker.js"]
-        resource url: [dir: "js/datepicker", file:"jquery.timepicker.min.js"]
+        resource url: [dir: "css/datepicker", file: "bootstrap-datepicker.css", plugin: "nucleus"]
+        resource url: [dir: "css/datepicker", file: "jquery.timepicker.css", plugin: "nucleus"]
+        resource url: [dir: "js/datepicker", file: "bootstrap-datepicker.js", plugin: "nucleus"]
+        resource url: [dir: "js/datepicker", file:"jquery.timepicker.min.js", plugin: "nucleus"]
     }
     trackTimeZone {
-        resource url: [dir: 'js', file: "track-time-zone.js"]
+        resource url: [dir: 'js', file: "track-time-zone.js", plugin: "nucleus"]
     }
 }
