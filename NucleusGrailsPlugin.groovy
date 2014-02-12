e3feddc80a8f8f20c77a37e566234e330bf835e7
Shashank Agrawal <shashank.agrawal@causecode.com>
2014-02-12 19:26:25 +0530

    Released final release 0.2

--- NucleusGrailsPlugin.groovy
+++ NucleusGrailsPlugin.groovy
@@ -8,7 +8,7 @@
 
 class NucleusGrailsPlugin {
 
-    def version = "0.2-SNAPSHOT"
+    def version = "0.2"
     def grailsVersion = "2.2 > *"
     def groupId = "com.cc.plugins"
     def dependsOn = [:]
