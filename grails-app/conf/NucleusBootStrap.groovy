0729a6d42ddb48d55e9bb05e50122c2c2c4a560f
Shashank Agrawal <shashank.agrawal@causecode.com>
2014-11-28 11:30:51 +0530

    Added some String utility methods.

--- grails-app/conf/NucleusBootStrap.groovy
+++ grails-app/conf/NucleusBootStrap.groovy
@@ -21,6 +21,8 @@ class NucleusBootStrap {
     def init = { servletContext ->
         log.debug "Nucleus Bootstrap started executing .."
 
+        registerStringUtilityMethods()
+
         NucleusUtils.initialize()
 
         XML.registerObjectMarshaller(new SitemapMarshaller())
@@ -32,4 +34,40 @@ class NucleusBootStrap {
         log.debug "Nucleus Bootstrap finished executing."
     }
 
+    private static void registerStringUtilityMethods() {
+        /**
+         * Used to convert camel case to title or natural case.
+         * @example "firstName".camelCaseToTile() == "First Name"
+         */
+        String.metaClass.camelCaseToTile = {
+            String title = delegate.replaceAll(/\B[A-Z]/) { " " + it }.capitalize()
+            return title
+        }
+
+        /**
+         * Used to convert hyphenated string to camel case.
+         * @Example "some-name".toUpperCamelCase() == "someName"
+         */
+        String.metaClass.toUpperCamelCase = {
+            return delegate.replaceAll("(-)([A-Za-z0-9])", { Object[] value ->
+                value[2].toUpperCase()
+            })
+        }
+
+        /**
+         * Used to convert camel case string to hyphenated/snaked case.
+         * @Example "someName".toSnakeCase() == "some-name"
+         */
+        String.metaClass.toSnakeCase = {
+            return delegate.replaceAll(/([A-Z])/, /-$1/).toLowerCase().replaceAll(/^-/, "")
+        }
+
+        /**
+         * Used to capitalize first character of each word in a string.
+         * @Example "some word".capitalizeAll() == "Some Word"
+         */
+        String.metaClass.capitalizeAll = {
+            return delegate.split(" ").collect{ it.capitalize() }.join(" ")
+        }
+    }
 }
