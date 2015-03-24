1de0650c985bb29097dcc456c2f2be73b8053935
Rachna Chauhan <rachna.chauhan@causecode.com>
2015-03-24 17:50:02 +0530

    Modifications done for upgradation of Nucleus from Grails 2.3 to Grails 2.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,17 +17,20 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
+        test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
     }
 
     plugins {
-        build(":tomcat:7.0.42", ":release:3.0.1", ":rest-client-builder:2.0.1") {
+        build(":tomcat:7.0.55", ":release:3.0.1", ":rest-client-builder:2.0.1") {
             export = false
         }
         compile (":spring-security-core:2.0-RC3") {
             export = false
         }
-        /*compile (":hibernate:3.6.10.7") {
+        compile (":hibernate:3.6.10.18") {
             export = false
-        }*/
+        }
+        //plugins for compile step
+        compile ':cache:1.1.3'
     }
 }
