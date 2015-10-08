265c8a36e5a63bf1e314498f2d2aa75756a8cc12
Rachna Chauhan <rachna.chauhan@causecode.com>
2015-10-08 11:45:15 +0530

    Sitemap related changes.
    Conflicts:
    	grails-app/conf/NucleusUrlMappings.groovy
    	grails-app/domain/com/cc/seo/sitemap/Sitemap.groovy

--- grails-app/controllers/com/cc/seo/sitemap/SitemapController.groovy
+++ grails-app/controllers/com/cc/seo/sitemap/SitemapController.groovy
@@ -10,7 +10,6 @@ package com.cc.seo.sitemap
 
 import grails.plugin.springsecurity.annotation.Secured
 
-
 @Secured(["ROLE_ADMIN"])
 class SitemapController {
 
@@ -31,12 +30,15 @@ class SitemapController {
         redirect uri: "/dashboard"
     }
 
-    /**
+   /**
      * Default action : Responds Sitemap first object or new sitemap Object.
+     * Reverting back the commit: https://bitbucket.org/causecode/nucleus/commits/a4500461f761f69bcf39372d03a5b3fe58b84dce
+     * Replaced respond with render, since it was adding an extra xml <list> element to the generated
+     * sitemap.xml
      */
     @Secured(["permitAll"])
     def index() {
-        respond Sitemap.first() ?: EMPTY_SITEMAP
+        render((Sitemap.first() ?: EMPTY_SITEMAP) as XML)
     }
 
 }
