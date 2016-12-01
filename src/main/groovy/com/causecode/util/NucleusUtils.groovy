beae1723942c22874f84bf9ee9eeac8f2e22fe99
Ankit Agrawal <ankit.agrawal@causecode.com>
2016-12-01 12:37:03 +0530

    Added support for overriding configurations using external file.

--- src/main/groovy/com/causecode/util/NucleusUtils.groovy
+++ src/main/groovy/com/causecode/util/NucleusUtils.groovy
@@ -7,12 +7,15 @@
  */
 package com.causecode.util
 
+import grails.boot.config.GrailsAutoConfiguration
 import grails.util.Environment
+import org.springframework.core.env.Environment as SEnvironment
 import grails.util.Holders
 import org.springframework.beans.BeansException
 import org.springframework.context.ApplicationContext
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
+import org.springframework.core.env.MapPropertySource
 
 /**
  * A utility class to provide some common & useful stuff
@@ -120,6 +123,50 @@ class NucleusUtils {
         }
 
         logger.debug "Exception email sent"
+    }
+
+    /**
+     * Method used to override configurations from an external file. This method can be called from any application that
+     * wants to override it's application configuration properties using an external config file.
+     * Location of external config files are picked from application.groovy file of parent application.
+     * Locations are to be provided under 'grails.config.locations'
+     *
+     * example:
+     * Environment specific config override file locations.
+     * environments {
+     *     production {
+     *         grails.config.locations = ['path/to/application-local.groovy']
+     *     }
+     *     development {
+     *         grails.config.locations = ['path/to/application-local.groovy']
+     *     }
+     *     test {
+     *         grails.config.locations = ['path/to/application-local.groovy']
+     *     }
+     *     staging {
+     *         grails.config.locations = ['path/to/application-local.groovy']
+     *     }
+     * }
+     *
+     * note: config override depends on order of passes file locations.
+     *
+     * @params application
+     * @params org.springframework.core.env.Environment environment
+     *
+     * @since 0.4.1
+     * @author Ankit Agrawal
+     */
+    static void addExternalConfig(GrailsAutoConfiguration application, SEnvironment environment) {
+        URL applicationGroovy = application.getClass().classLoader.getResource('application.groovy')
+        if (applicationGroovy) {
+            ConfigObject applicationConfiguration = new ConfigSlurper(Environment.current.name).parse(applicationGroovy)
+
+            (applicationConfiguration.grails.config.locations).each { String configurationResource ->
+                ConfigObject config = new ConfigSlurper(Environment.current.name)
+                        .parse(new File(configurationResource).toURI().toURL())
+                environment.propertySources.addFirst(new MapPropertySource('external-config', config))
+            }
+        }
     }
 
     static void sendExceptionEmail(Throwable exception, Map model) {
